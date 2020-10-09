a39e0ae220c619a38d0b3f0f186b696ce47efd4c
Miguel Ojeda <46506567+MiguelBim@users.noreply.github.com>
2020-10-09 13:14:50 -0500

    Adding dot env integration

--- SAS_Server/data_consolidation.py
+++ SAS_Server/data_consolidation.py
@@ -3,35 +3,41 @@ import pandas as pd
 import tempfile
 import warnings
 import csv
+from dotenv import load_dotenv
+import os
 
-
-warnings.filterwarnings("ignore")
 pd.set_option('display.max_rows', 700)
 pd.set_option('display.max_columns', 700)
 pd.set_option('display.width', 1000)
 
+load_dotenv()
+SMB_USER = os.getenv("SMB_USER")
+SMB_PASSWORD = os.getenv("SMB_PASSWORD")
+SMB_SERVER_NAME = os.getenv("SMB_SERVER_NAME")
+SMB_SERVER_IP = os.getenv("SMB_SERVER_IP")
+warnings.filterwarnings("ignore")
+
 
 # FUNCTION TO FILTER MEANINGFUL FILES PATHS FROM BIGID INVENTORY REPORT
 def extract_clarity_reports(report_name, sas_folder):
-
     df = pd.read_csv(report_name)
     reduced_df = df[['Full Object Name', 'Object Name', 'PII Count']]
     filtered_reduced_df = reduced_df[reduced_df['Full Object Name'].str.contains("clarity_inquiry")]
     filtered_reduced_df['Full Object Name'] = filtered_reduced_df['Full Object Name'].map(lambda x: x.lstrip(sas_folder))
-    filtered_reduced_df.to_csv('clarity_files_paths.csv', header=True, index=True)
+    # filtered_reduced_df.to_csv('clarity_files_paths.csv', header=True, index=True)
 
     return filtered_reduced_df
 
 
 # FUNCTION TO READ AND RETRIEVE FILES FROM SAS SERVER
 def retrieve_file_fromsmb(service_name, path):
-
-    user_name = "XXXX"
-    password = "XXXX"
-    local_machine_name = "XXXX"
-    server_machine_name = "XXXX"
-    server_ip = "XXXX"
+    user_name = SMB_USER
+    password = SMB_PASSWORD
+    local_machine_name = "laptop"
+    server_machine_name = SMB_SERVER_NAME
+    server_ip = SMB_SERVER_IP
     path = path.replace('\\', '/')
+
 
     print("\nFile retrieval process was requested for file -> " + path)
     try:
@@ -39,21 +45,26 @@ def retrieve_file_fromsmb(service_name, path):
         assert conn.connect(server_ip, 445)
         tmp = tempfile.NamedTemporaryFile(delete=True)
         conn.retrieveFile(service_name, path, tmp)
-        df = pd.read_csv(tmp.name)
+        chunksize = 100000
+        try:
+            tfr = pd.read_csv(tmp.name, chunksize=chunksize,
+                              iterator=True, dtype=str, error_bad_lines=False)
+        except:
+            tfr = pd.read_csv(tmp.name, chunksize=chunksize, quoting=csv.QUOTE_NONE,
+                              iterator=True, dtype=str, error_bad_lines=False)
+
+        df = pd.concat(tfr, ignore_index=True)
+
         return df, path
+
     except:
         print("\t---> File retrieval process was FAILED for file -> " + path)
-
-    # df.to_csv('readed_file.csv', header=True, index=True)
 
 
 # FUNCTION CREATED FOR CONSOLIDATION OF ALL CLARITY_INQUIRY FILES
 def consolidate_data(bigid_rep_df, service_name):
-
     consolidate_df = pd.DataFrame([])
-
     for path in bigid_rep_df['Full Object Name']:
-
         try:
             sas_file_df, real_path = retrieve_file_fromsmb(service_name, path)
             sas_df_subset = sas_file_df[['social_security_number', 'state']]
@@ -64,16 +75,27 @@ def consolidate_data(bigid_rep_df, service_name):
             print("\t---> Process of data consolidation was done successfully for file -> " + real_path)
         except:
             print("\t---> Process of data consolidation FAILED for file -> " + path)
-
     consolidate_df = consolidate_df[consolidate_df['social_security_number'] != 0]
-    consolidate_df.drop_duplicates(subset='social_security_number').reset_index(drop=True)
-    consolidate_df.to_csv('data_consolidation_details.csv', header=True, index=True)
+    final_df = consolidate_df.drop_duplicates(subset='social_security_number', keep='first').reset_index(drop=True)
+    final_df.to_csv('data_consolidation_details.csv', header=True, index=True)
+
+    return
+
+
+def filter_by_states(df, states):
+
+    try:
+        filtered_df = df.loc[df['state'].isin(states)]
+        filtered_df.insert(loc=0, column='Num', value=range(1, len(filtered_df) + 1))
+        filtered_df.drop_duplicates(subset='social_security_number')
+        filtered_df.to_csv('data_consolidation (Others).csv', header=True, index=False)
+    except:
+        print('There was an issue when filtering for those values. Please check')
 
     return
 
 
 if __name__ == '__main__':
-
     bigid_rep_name = "Objects (DS).csv"
     sas_server = 'Workspaces'
 
@@ -83,3 +105,8 @@ if __name__ == '__main__':
 
     # CREATE UNIFIED DATAFRAME AUTOMATICALLY
     consolidate_data(clarity_files_path, sas_server)
+
+    # FILTER BY STATES (LIST OF VALUE) THE CONSOLIDATION FILE
+    df = pd.read_csv("data_consolidation_details.csv", index_col=0)
+    states = ['CA','TX']
+    filter_by_states(df, states)
