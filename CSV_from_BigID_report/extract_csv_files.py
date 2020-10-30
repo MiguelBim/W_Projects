c71cc148781108c956ea218e56df4763ce556dca
Miguel Ojeda <46506567+MiguelBim@users.noreply.github.com>
2020-10-30 15:43:38 -0600

    Files filter introduced

--- CSV_from_BigID_report/extract_csv_files.py
+++ CSV_from_BigID_report/extract_csv_files.py
@@ -11,6 +11,18 @@ def extract_csv_txt_files(report_name):
     return
 
 
+def extract_paths_from_pattern(report_name, pattern):
+
+    report_df = pd.read_csv(report_name)
+    filtered_df = report_df[report_df['Full Object Name'].str.contains(pattern)].reset_index(drop=True)
+    print(filtered_df)
+    filtered_df.to_csv('Pattern_Objects.csv', index=False)
+
+    return
+
+
 if __name__ == '__main__':
-    report_name = 'Objects.csv'
-    extract_csv_txt_files(report_name)
+    report_name = 'Fraud.csv'
+    pattern = 'fraudcheck'
+    # extract_csv_txt_files(report_name)
+    extract_paths_from_pattern(report_name, pattern)
